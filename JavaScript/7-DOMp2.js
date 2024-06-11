9096df7227451d2e1e10dec3e70c400a0449ad49
Zero-7-1 <109064402+Zero-7-1@users.noreply.github.com>
2024-06-11 12:17:59 +0530

    way to completion

--- JavaScript/7-DOMp2.js
+++ JavaScript/7-DOMp2.js
@@ -5,7 +5,7 @@ kind of event, we would look events after this but here we focus on what will ha
 when an event is triggered i.e. insert, delete, etc. 
 */
 
-// Q. HOW CAN WE ADD AN ELEMENT THROUGH DOM TO THE DOCUMENT  ? 
+// Q. HOW CAN WE ADD OR CREATE AN ELEMENT THROUGH DOM TO THE DOCUMENT  ? 
 const div = document.createElement('div'); // a div has been created in memory 
 div.className = 'my-element'; // created class 
 div.id = 'element-id';
@@ -26,4 +26,80 @@ console.log(div);
 document.body.appendChild(div); // this will be added to buttom of document
                                 // after everything else  
 
-// WHAT'S THE DIFFERENCE BETWEEN INNERHTML VS CREATEELEMENT ? 
+
+// innerHTML VS CreateElement 
+/*This is a way of adding a item to our shopping lister DOM mini project
+  uisng innerHTML have a look  */
+function createListItem(item) {
+      const li = document.createElement('li');
+      li.innerHTML = `${item}
+          <button class="remove-item btn-link text-red">
+            <i class="fa-solid fa-xmark"></i>
+          </button>`;
+
+      document.querySelector('.items').appendChild(li);
+}
+createListItem('Eggs');
+
+/* Better alternative method of above creating every element, this one is more
+ efficient way then the other one as for first one browser will have parse
+ everytime it loads and it may problems with event handlers  */
+
+
+function createNewListItem(item) {
+   const li = document.createElement('li');
+   li.appendChild(document.createTextNode(item));
+   const button = document.createElement('button');
+   button.className = 'remove-item btn-link text-red';
+   const icon = document.createElement('i');
+   icon.className ='fa-solid fa-xmark';
+   button.appendChild(icon); // icons needs to appended inside button 
+   li.appendChild(button); // similarly button needs to go inside list 
+
+   
+   document.querySelector('.items').appendChild(li);
+};
+createNewListItem('Curd');
+
+// Even much better resusable way (Refactor to Multiple Elements)
+
+function createNewItem(item) {
+      const li = document.createElement('li');
+      li.appendChild(document.createTextNode(item));
+      const button2 = createButton('remove-item btn-link text-red');
+
+      li.appendChild(button2); 
+   
+      
+      document.querySelector('.items').appendChild(li);
+   };
+
+function createButton(classes) {
+      const button2 = document.createElement('button');
+      button2.className = classes;
+      
+      const icon2 = createIcon('fa-solid fa-xmark');
+      button2.appendChild(icon2); 
+      
+      return button2;
+};
+
+function createIcon(classes) {
+      const icon2 = document.createElement('i');
+      icon2.className = classes;
+      return icon2;
+};
+createNewItem('Chicken');
+createNewItem('Ketchup');
+/*This more efficient way, we have separate icon and button methods to create buttons 
+  and icons.We are setting the button to the return value of create button function
+   , appending it to  list item and then ultimatley we are appending list item to 
+   the ul in dom  */
+
+
+
+   // insert Elements, Text & HTML Other methods different to appendChild 
+     // inertAdjacentText 
+     // insertAdjacentHTML
+     // insertBefore
+     // time stamp -- 9hr:25mins 
